c0033ade5613eb7548a007dc111c1d488d4ea2bb
imlonghao <shield@fastmail.com>
2016-01-31 18:12:20 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name='cachet',
+        version='0.1.0',
+        url='https://github.com/imlonghao/cachet.python',
+        license='MIT',
+        author='imlonghao',
+        author_email='shield@fastmail.com',
+        description='Python client library for the Cachet API',
+        long_description=long_description,
+        py_modules=['cachet'],
+        install_requires=['requests'],
+        platforms='any'
+)
