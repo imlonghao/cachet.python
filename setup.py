9b1608bb980c2405c322a0aa2bb2cc7b4e05df1a
imlonghao <git@imlonghao.com>
2019-06-05 14:05:58 +0800

    New version: v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
         name='cachet',
-        version='0.1.0',
+        version='0.1.1',
         url='https://github.com/imlonghao/cachet.python',
         license='MIT',
         author='imlonghao',
